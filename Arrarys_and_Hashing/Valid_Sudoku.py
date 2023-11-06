34c0cfe91b140d4399b90a35655a934418baba67
milayaryna <70626518+milayaryna@users.noreply.github.com>
2023-11-06 16:35:50 +0800

    Create Valid_Sudoku.py

---
+++ Arrarys_and_Hashing/Valid_Sudoku.py
@@ -0,0 +1,46 @@
+# Valid Sudoku
+
+'''
+Determine if a 9 x 9 Sudoku board is valid.
+
+Example 1:
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+Example 2:
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+'''
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        res = []
+        for i in range(9):
+            for j in range(9):
+                element = board[i][j]
+                if element != '.':
+                    res += [(i, element), (element, j), (i//3, j//3, element)]
+                    # (i, element): row index and element
+                    # (element, j): element and column index
+                    # (i//3, j//3, element): the 3x3 sub-grid that the current cell belongs to
+        return len(res) == len(set(res))
+
