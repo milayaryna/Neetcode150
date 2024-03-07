5765a8d8947870219d3dab96da9aecf9de111217
milayaryna <70626518+milayaryna@users.noreply.github.com>
2024-03-07 11:58:45 +0800

    Update Subtree_of_Another_Tree.py

--- Trees/Subtree_of_Another_Tree.py
+++ Trees/Subtree_of_Another_Tree.py
@@ -15,3 +15,24 @@ Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
 Output: false
 '''
 
+
+'''
+The isSameTree function is taken directly from that problem - Same Tree.
+We want to figure out if any of the subtrees of root are the same tree as subroot.
+'''
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root:
+            return False
+        if self.isSameTree(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
